a51e9567f8ab60817f989477b75458e0839e6ea4
Chen chen <43808882+bitifyChen@users.noreply.github.com>
2026-03-31 12:31:35 +0800

    path setting

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/Coolkertour-Silver/',
   plugins: [
     vue(),
     vueDevTools(),
